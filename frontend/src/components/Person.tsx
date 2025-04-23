6f3239c3bb15bb238d9065ce7d10fa6d9c0fd47f
mcaland <maxwellcalandro@gmail.com>
2025-04-23 19:47:05 -0400

    changed Person component to take in parameters for users

--- frontend/src/components/Person.tsx
+++ frontend/src/components/Person.tsx
@@ -9,8 +9,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Image from '../assets/fake_person.png';
 
 
-function Person()
+function Person({ name = "John Doe", experienceTags = [], lookingforTags = [] })
 {
+    function parseTags(e : string[])
+    {
+        let tagList : any[] = [];
+
+        for (let i = 0; i < e.length; i++)
+        {
+            tagList.push(<Button style={{flex: '0 0 auto'}}>{e[i]}</Button>);
+        }
+
+        return tagList;
+    }
+
     return (
     <Col>
         <Card style={{width: '20rem'}} data-bs-theme='dark'>
@@ -18,25 +30,19 @@ function Person()
             <Card.Body>
                 <Stack>
                     <Card.Title>
-                        John Doe
+                        {name}
                     </Card.Title>
                     <Card.Text>
                         looking for:
                     </Card.Text>
                     <Stack direction='horizontal' style={{overflowX: "scroll", overflowY: 'hidden', width: '100%', display: 'flex', gap: '5px', paddingBottom: '5px'}}>
-                        <Button style={{flex: '0 0 auto'}}>guitarist</Button>
-                        <Button style={{flex: '0 0 auto'}}>drummer</Button>
-                        <Button style={{flex: '0 0 auto'}}>guitarist</Button>
-                        <Button style={{flex: '0 0 auto'}}>drummer</Button>
-                        <Button style={{flex: '0 0 auto'}}>guitarist</Button>
-                        <Button style={{flex: '0 0 auto'}}>drummer</Button>
+                        {parseTags(lookingforTags)}
                     </Stack>
                     <Card.Text>
                         skills:
                     </Card.Text>
                     <Stack direction='horizontal' style={{overflowX: "scroll", overflowY: 'hidden', width: '100%', display: 'flex', gap: '5px', paddingBottom: '5px'}}>
-                        <Button style={{flex: '0 0 auto'}}>guitar 3 years</Button>
-                        <Button style={{flex: '0 0 auto'}}>drums 1 year</Button>
+                        {parseTags(experienceTags)}
                     </Stack>
                     <Button>Interested</Button>
                 </Stack>

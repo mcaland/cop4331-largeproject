e01930637047fd1fbac5019566034962247b7e54
BrandonSheridan <bhsheridan2@gmail.com>
2025-04-24 16:19:33 -0400

    added functionality to SearchOutput.tsx, needs testing

--- frontend/src/components/SearchOutput.tsx
+++ frontend/src/components/SearchOutput.tsx
@@ -9,9 +9,21 @@ import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Button from 'react-bootstrap/Button';
 import Alert from 'react-bootstrap/Alert';
 import handleChange from './Search.tsx';
+import Person from "../components/Person";
 
 async function SearchOutput()
 {
+
+    
+
+    const [err, updateError] = React.useState('');
+
+    function updateErrorMessage(e : string)
+    {
+        updateError(e);
+
+    }
+
     var obj = {keyword: handleChange};
     var js = JSON.stringify(obj);
 
@@ -25,11 +37,35 @@ async function SearchOutput()
             {
                 updateErrorMessage(res.error);
             }
-        }
+            else
+            {
+                var users = {
+                    displayName: res.displayName,
+                    skills: res.skills,
+                    lookingFor: res.lookingFor,
+                    imageUrl: res.imageUrl,
+                    audioUrl: res.audioUrl
+                };
+                
+                
+                <Person 
+                    {...users.displayName}
+                    {...users.skills}
+                    {...users.lookingFor}
+                    {...users.imageUrl}
+                    {...users.audioUrl}
+                />
+        
+            }
 
-    finally {return(
-        <Person />
-    )}
+        }
+        catch (error : any)
+        {
+            updateErrorMessage(error.toString());
+            return;
+        }
+           
 }
+
 
 export default SearchOutput;
